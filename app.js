5b2dfe5e390edad9fa38b442c8e55e739119478f
Gabriel Pontes <gabrielpontescontato1@gmail.com>
2022-07-30 18:58:26 -0300

    Second commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+//View Engine
+app.set('view engine','ejs');
+
+//Arquivos Estáticos
+app.set(express.static('public'));
+
+// Rotas
+app.get('/',(req,res) => {
+    res.render("login");
+});
+
+app.listen(PORT, () => {
+    console.log('Servidor iniciado em http://localhost:' + PORT);
+});
+
+//Data Base
+(async () => {
+    const database = require('./database/database');
+
+    try {
+      const resultado = await database.sync();
+      console.log(resultado);
+    } catch (error) {
+        console.log(error);
+    }
+})();
