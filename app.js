b173af7cbc6e2140fad8453c7d343be69faca614
Gabriel Pedro Pereira Torres <gabriel.torres@renova.com.br>
2022-08-03 16:34:26 -0300

    Tela de login e Esqueci minha senha

--- app.js
+++ app.js
@@ -3,28 +3,31 @@ const app = express();
 const PORT = 8080;
 
 //View Engine
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 
 //Arquivos Estáticos
-app.set(express.static('public'));
+app.set(express.static("public"));
 
 // Rotas
-app.get('/',(req,res) => {
-    res.render("login");
+app.get("/", (req, res) => {
+  res.render("login");
+});
+app.get("/Esquecirsenha", (req, res) => {
+  res.render("Esquecirsenha");
 });
 
 app.listen(PORT, () => {
-    console.log('Servidor iniciado em http://localhost:' + PORT);
+  console.log("Servidor iniciado em http://localhost:" + PORT);
 });
 
 //Data Base
 (async () => {
-    const database = require('./database/database');
+  const database = require("./database/database");
 
-    try {
-      const resultado = await database.sync();
-      console.log(resultado);
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const resultado = await database.sync();
+    console.log(resultado);
+  } catch (error) {
+    console.log(error);
+  }
 })();
